0399937a2a2c8cfd55dca5167f1a8c109d5a254b
Casey Cameron <mcasey.cameron@gmail.com>
2021-07-15 16:25:14 -0700

    Bot awaits answer for the image flag

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import Discord, { User } from 'discord.js';
-import { search, city, sendImages, sendChannelResults, sendUserResults } from './lib/utils/discord-utils.js';
+import { search, city, checkForImages, sendChannelResults, sendUserResults } from './lib/utils/discord-utils.js';
 
 export const client = new Discord.Client();
 
@@ -20,16 +20,15 @@ client.on('message', async (message) => {
   if (message.content === '!scrape') {
     message.channel.send('Reply with "!scrape <search term> -c <city> -i for images');
   } else if (message.content.startsWith('!scrape ')) {
-
-    imageCheck = sendImages(message.content);
+    imageCheck = checkForImages(message);
     searchTerm = search(message.content);
-    cityTerm = city(message.content);
-  }
-  if (!imageCheck) {
-    message.channel.send(await sendChannelResults(searchTerm, cityTerm));
-  } else {
-    message.channel.send(`${message.author}, please check your DMs.`);
-    sendUserResults(searchTerm, cityTerm, message);
+    cityTerm = city(message);
+
+    if (!imageCheck) {
+      message.channel.send(await sendChannelResults(searchTerm, cityTerm));
+    } else {
+      sendUserResults(searchTerm, cityTerm, message);
+    }
   }
 });
 
